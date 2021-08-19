faca818c72033f8d9fe30397fa255c74dc77a463
max <mfahampshire@gmail.com>
2021-08-20 00:27:43 +0200

    added mass minting script for testnet

---
+++ scripts/mint-mass.js
@@ -0,0 +1,53 @@
+const Metadata = artifacts.require('./Metadata.sol');
+const FlowerToken = artifacts.require('./FlowerToken.sol');
+const LegacyFlowerToken = artifacts.require('./buyable.sol');
+const contracts = require('../contractAddresses.json');
+require('dotenv').config()
+
+module.exports = async function(callback) {
+
+const user = process.env.RINKEBYACCOUNT;
+const legacy = await LegacyFlowerToken.at(contracts.rinkeby.LEGACY);
+const flowertoken = await FlowerToken.at(contracts.rinkeby.FLOWERTOKENS);
+
+    try {
+
+        console.log(legacy.address);
+        console.log(flowertoken.address); 
+        console.log(user);
+
+        let i; 
+        for(i=2;i<30;i++) {
+            console.log('minting legacy');
+            await legacy._mint(user, i, { from: user }); 
+            console.log('legacy minted, approving legacy -> flowertokens transfer');
+            await legacy.approve(flowertoken.address,i, { from: user });
+            console.log('approved, converting token');
+            let tx = await flowertoken.convertToNew(i, { from: user });
+            console.log('token converted to new token!')
+        }
+
+        // console.log('minting legacy');
+        // await legacy._mint(user, 1, { from: user }); 
+        // console.log('legacy minted, approving legacy -> flowertokens transfer');
+        // await legacy.approve(flowertoken.address,1, { from: user });
+        // console.log('approved, converting token');
+        // let tx = await flowertoken.convertToNew(1, { from: user });
+        // console.log('token converted to new token!')
+        // console.log(tx); 
+
+    } catch (error) {
+        console.log(error);
+    }
+
+    callback();
+
+}
+
+/*
+- mint legacy 
+- approve newone to be sold by newTokenScripts 
+- convertToNew
+- check opensea 
+*/
+
